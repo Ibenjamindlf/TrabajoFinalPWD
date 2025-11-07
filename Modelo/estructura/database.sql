9db7dd547f918ab68f5671f58533fa3b2aeda4e2
matias bacsay <149085169+tebacs@users.noreply.github.com>
2025-11-07 00:24:18 -0300

    tabla producto sql

--- Modelo/estructura/database.sql
+++ Modelo/estructura/database.sql
@@ -1,8 +1,13 @@
 CREATE DATABASE db_ecommerce_PWD;
 USE db_ecommerce_PWD;
 
-CREATE Table productos (
-    id INT(10) NOT NULL AUTO_INCREMENT,
+-- 1. Tabla PRODUCTO
+CREATE TABLE producto (
+    id INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(250) NOT NULL,
+    stock INT NOT NULL,
+    precio DECIMAL(10,2) NOT NULL,
+    detalle TEXT NOT NULL,
+    imagen VARCHAR(255) NOT NULL
     PRIMARY KEY (id)
-)
+);
