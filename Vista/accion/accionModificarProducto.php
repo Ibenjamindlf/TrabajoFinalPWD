9c987e538217a2798691c1fba8a2f3c0d316f275
Benjamin de la fuente <206235917+Ibenjamindlf@users.noreply.github.com>
2025-11-10 00:09:08 -0300

    Update accionModificarProducto.php
    Modificaciones en la accionModificarProducto.php para que nos deje modificar el producto y a su vez eliminar la imagen con el fin de economizar espacios y/o crear su nueva imagen dentro de la carpeta.

--- Vista/accion/accionModificarProducto.php
+++ Vista/accion/accionModificarProducto.php
@@ -2,20 +2,59 @@
 include_once ("../../Control/abmProducto.php");
 include_once ("../../utilidades/funciones.php");
 
-// $datos = $_POST;
+$datos = data_submitted();
 $abmProducto = new ABMProducto();
 
-// print_r($datos);
-$seModifico = $abmProducto->modificacion(data_submitted());
+$hayImg = $datos['imagen']['name'];
+// Si el usuario cambio la foto
+if ($hayImg != null){
 
+    $producto = $abmProducto->buscar(['id' => $datos['id']]);
 
-if ($seModifico){
-    $message = 'Se modifico correctamente el producto';
-    header("Location: ../admin/panelAdmin.php?Message=" . urlencode($message));
-    exit;
+    $producto = $producto[0];
+
+    $rutaImg = $producto->getimagen();
+
+    $rutaFisica = "../../" . $rutaImg;
+
+    unlink($rutaFisica);
+
+    // Configuracion del directorio de destino
+    $uploadDir = "../sources/img/";
+
+    // Datos del archivo subido
+    $archivo = $datos['imagen'];
+    $nombreArchivo = basename($archivo['name']); // basename() evita path traversal
+    $rutaDestino = $uploadDir . $nombreArchivo;
+
+    // --- Mover archivo subido ---
+    if (move_uploaded_file($archivo['tmp_name'], $rutaDestino)) {
+
+        // Guardamos la ruta RELATIVA que se usará desde la web (por ejemplo, para mostrar imagen en el front)
+        $datos['imagen'] = "Vista/sources/img/" . $nombreArchivo;
+
+        // --- Insertar producto en BD ---
+        $seModifico = $abmProducto->modificacion($datos);
+
+    } else {
+        $message = 'Error al subir la imagen.';
+    }
 } else {
-    $message = 'Hubo un error al modificar el producto';
-    header("Location: ../admin/panelAdmin.php?Message=" . urlencode($message));
-    exit;
+    $producto = $abmProducto->buscar(['id' => $datos['id']]);
+    if ($producto) {
+        $rutaImg = $producto[0]->getImagen();
+    }
+    // echo $rutaImg;
+    $datos['imagen'] = $rutaImg;
+    // print_r($datos);
+    $seModifico = $abmProducto->modificacion($datos);
 }
+if ($seModifico) {
+    $message = 'Se modifico correctamente el producto.';
+} else {
+    $message = 'Hubo un error al modifico el producto.';
+}
+// --- Redirección con mensaje ---
+header("Location: ../admin/panelAdmin.php?Message=" . urlencode($message));
+exit;
 ?>
