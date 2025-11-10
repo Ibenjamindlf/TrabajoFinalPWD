601f6e95bf871714d57a3328e0b7cff44fe25617
Benjamin de la fuente <206235917+Ibenjamindlf@users.noreply.github.com>
2025-11-10 00:08:30 -0300

    Update accionEliminarProducto.php
    Modificaciones en la accionEliminarProducto.php para que nos deje eliminar el producto y a su vez eliminar la imagen con el fin de economizar espacios.

--- Vista/accion/accionEliminarProducto.php
+++ Vista/accion/accionEliminarProducto.php
@@ -2,21 +2,28 @@
 include_once ("../../Control/ABMProducto.php");
 include_once ("../../utilidades/funciones.php");
 
-// Nose ni por que deje use las 3 lineas de abajo pero por las dudas no las voy a borrar
-// print_r($_GET);
-// $idProducto = $_GET['id'];
-// print_r($idProducto);
+$datos = data_submitted();
 $abmProducto = new ABMProducto();
 
-$seElimino = $abmProducto->baja(data_submitted());
+$producto = $abmProducto->buscar(['id'=>$datos['id']]);
 
-if ($seElimino){
+if ($producto){
+    $producto = $producto[0];
+
+    $rutaImg = $producto->getimagen();
+    $rutaFisica = "../../" . $rutaImg;
+
+        unlink($rutaFisica);
+
+    $seElimino = $abmProducto->baja($datos);
+}
+
+if ($seElimino) {
     $message = 'Se elimino correctamente el producto';
-    header("Location: ../admin/panelAdmin.php?Message=" . urlencode($message));
-    exit;
 } else {
     $message = 'Hubo un error al eliminar el usuario';
-    header("Location: ../admin/panelAdmin.php?Message=" . urlencode($message));
-    exit;
 }
+
+header("Location: ../admin/panelAdmin.php?Message=" . urlencode($message));
+exit;
 ?>
