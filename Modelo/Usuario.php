97e174065bd8e2f254ea5245eb8462a3fd3ac79e
tebacs <matibacsay@gmail.com>
2025-11-11 15:52:11 -0300

    alter Usuario.php

--- Modelo/Usuario.php
+++ Modelo/Usuario.php
@@ -124,10 +124,121 @@ class Usuario {
         if ($base->Iniciar()) {
             if ($base->Ejecutar($sql) > 0) {
                 $resp = true;
+            }else {
+                $this->setMensajeOperacion("Usuario->insertar: " . $base->getError());
             }
         } else {
             $this->setMensajeOperacion("Usuario->insertar: " . $base->getError());
         }
         return $resp;
     }
+
+    /**
+     * Modulo modificar
+     * (funcion para modificar un usuario en la BD)
+     * @return bool
+     */
+    public function modificar() {
+        $resp = false;
+        $base = new Database();
+        $id = $this->getId();
+        $nombre = $this->getNombre();
+        $password = $this->getPassword();
+        $mail = $this->getMail();
+        $deshabilitado = $this->getDeshabilitado();
+        $sql = "UPDATE usuario SET 
+                nombre='$nombre', 
+                password='$password', 
+                mail='$mail', 
+                deshabilitado='$deshabilitado' 
+                WHERE id=$id";
+        if ($base->Iniciar()) {
+            if ($base->Ejecutar($sql) > 0) {
+                $resp = true;
+            } else {
+                $this->setMensajeOperacion("Usuario->modificar: " . $base->getError());
+            }
+        } else {
+            $this->setMensajeOperacion("Usuario->modificar: " . $base->getError());
+        }
+        return $resp;
+    }
+
+    /**
+     * Modulo estado
+     * (funcion encargada de actualizar el estado del borrado logico = cuando stock llegue a 0)
+     * @param string|null $param
+     * @return bool
+     */
+    public function estado($param = "") {
+        $resp = false;
+        $base = new Database();
+        $id = $this->getId();
+        $sql = "UPDATE usuario SET deshabilitado = '$param' WHERE id = $id";
+        if ($base->Iniciar()) {
+            if ($base->Ejecutar($sql)) {
+                $resp = true;
+            } else {
+                $this->setMensajeOperacion("Usuario->estado: " . $base->getError());
+            }
+        } else {
+            $this->setMensajeOperacion("Usuario->estado: " . $base->getError());
+        }
+        return $resp;
+    }
+
+
+    /**
+     * Modulo eliminar
+     * (funcion que permite eliminar un producto de la BD)
+     * @return bool
+     */
+    public function eliminar() {
+        $resp = false;
+        $base = new Database();
+        $id = $this->getId();
+        $sql = "DELETE FROM usuario WHERE id = $id";
+        if ($base->Iniciar()) {
+            if ($base->Ejecutar($sql) > 0) {
+                $resp = true;
+            } else {
+                $this->setMensajeOperacion("Usuario->eliminar: " . $base->getError());
+            }
+        } else {
+            $this->setMensajeOperacion("Usuario->eliminar: " . $base->getError());
+        }
+        return $resp;
+    }
+
+    /**
+     * Modulo seleccionar
+     * (modulo que permite seleccionar en la BD)
+     * @param string $condicion
+     * @return array
+     */
+    public static function seleccionar($condicion = "") {
+        $arreglo = array();
+        $base = new Database();
+        $sql = "SELECT * FROM usuario";
+        if ($condicion != "") {
+            $sql .= " WHERE " . $condicion;
+        }
+        $sql .= " ORDER BY id ";
+        if ($base->Iniciar()) {
+            $res = $base->Ejecutar($sql);
+            if ($res > -1) {
+                if ($res > 0) {
+                    while ($row = $base->Registro()) {
+                        $obj = new Usuario();
+                        $obj->setear($row['id'], $row['nombre'], $row['password'], $row['mail'], $row['deshabilitado']);
+                        array_push($arreglo, $obj);
+                    }
+                }
+            }
+        } else {
+            self::setMensajeOperacion("producto->seleccionar: " . $dataBase->getError());
+        }
+        return $arreglo;
+    }
+
 }
