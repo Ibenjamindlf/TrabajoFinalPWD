5e21560c3486eb501ed9f78d7c0afd72f485ca1a
Benjamin de la fuente <206235917+Ibenjamindlf@users.noreply.github.com>
2025-11-27 01:16:47 -0300

    Update login.php
    Se agrega el inicio de session mediante el obj session

--- Vista/login.php
+++ Vista/login.php
@@ -1,5 +1,6 @@
 <?php
-    session_start();
+    require_once __DIR__ . '/../Control/Session.php';
+    $sesion = new Session();
     include_once ('../Vista/structure/header.php');
 ?>
 <!DOCTYPE html>
